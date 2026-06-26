6f9b36a91b4c996a78fefdd6c8f51c87c9e5e769
nemanja <obradovicn133@gmail.com>
2026-06-26 13:33:11 +0200

    komunikacija za arduino dodata

--- src/my_robot_slam/my_robot_slam/komunikacija.py
+++ src/my_robot_slam/my_robot_slam/komunikacija.py
@@ -2,13 +2,15 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32MultiArray
+import serial
+import time
 
 class KomunikacijaArduinoNode(Node):
     def __init__(self):
         super().__init__("komunikacija_arduino")
         self.get_logger().info("Čvor za komunikaciju sa Arduinom je pokrenut.")
 
-        # Sluša oba motora
+        self.arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)         # Sluša oba motora
         self.levi_sub = self.create_subscription(
             Int32MultiArray,
             'podaci_levog_motora',
@@ -23,10 +25,33 @@ class KomunikacijaArduinoNode(Node):
 
     def levi_callback(self, msg):
         self.get_logger().info(f"Most primio LEVI motor: {msg.data} -> Spreman za UART/Arduino.")
-        # Ovde dolazi tvoj kôd za slanje preko serijskog porta (npr. import serial)
+        if len(msg.data) >= 3:
+            naredba = msg.data[0]
+            brzina = msg.data[1]
+            duzina = msg.data[2]
+            
+            # Pozivamo funkciju za slanje
+            odgovor = self.write_read(naredba, brzina, duzina)
+            self.get_logger().info(f"Arduino odgovorio: {odgovor}")
 
     def desni_callback(self, msg):
         self.get_logger().info(f"Most primio DESNI motor: {msg.data} -> Spreman za UART/Arduino.")
+        if len(msg.data) >= 3:
+            naredba = msg.data[0]
+            brzina = msg.data[1]
+            duzina = msg.data[2]
+            
+            # Pozivamo funkciju za slanje
+            odgovor = self.write_read(naredba, brzina, duzina)
+            self.get_logger().info(f"Arduino odgovorio: {odgovor}")
+
+    def write_read(self, naredba, brzina, duzina): 
+        poruka = f"{naredba},{brzina},{duzina}\n"
+        self.arduino.write(bytes(poruka, 'utf-8')) 
+        time.sleep(0.05) 
+        data = self.arduino.readline() 
+        return data
+
 
 def main(args=None):
     rclpy.init(args=args)
