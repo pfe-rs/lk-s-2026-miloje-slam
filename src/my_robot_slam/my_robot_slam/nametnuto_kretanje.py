85de73ac008ca99b11c67e04c37eb7731a18d181
nemanja <obradovicn133@gmail.com>
2026-06-30 18:43:31 +0200

    popravka skretanja opet

--- src/my_robot_slam/my_robot_slam/nametnuto_kretanje.py
+++ src/my_robot_slam/my_robot_slam/nametnuto_kretanje.py
@@ -39,18 +39,21 @@ class NametnutoKretanjeNode(Node):
         self.kretanje_pub.publish(poruka)
 
     def okreni_robota(self, ciljni_ugao):
-        """Okreće robota na željeni pravac (0, 90, 180, 270)"""
+        """Okreće robota na željeni pravac prateći hardverske smerove"""
         razlika = (ciljni_ugao - self.trenutna_orijentacija) % 360
         
         if razlika == 0:
             return  # Već gleda u dobrom pravcu
             
-        if razlika == 90:    # Okret ulevo
+        if razlika == 90:    # Okret ulevo (Kontra-kazaljka)
+            # Levi ide unazad (+), desni ide unapred (+)
             self.posalji_komandu(STEPOVI_ZA_90_STEPENI, STEPOVI_ZA_90_STEPENI)
-        elif razlika == 270:  # Okret udesno (360 - 270 = 90 udesno)
+        elif razlika == 270:  # Okret udesno (Smer kazaljke)
+            # Levi ide unapred (-), desni ide unazad (-)
             self.posalji_komandu(-STEPOVI_ZA_90_STEPENI, -STEPOVI_ZA_90_STEPENI)
         elif razlika == 180:  # Okret za 180 stepeni
-            self.posalji_komandu(STEPOVI_ZA_90_STEPENI * 2, STEPOVI_ZA_90_STEPENI * 2)
+            # Dupli okret udesno
+            self.posalji_komandu(-STEPOVI_ZA_90_STEPENI * 2, -STEPOVI_ZA_90_STEPENI * 2)
             
         self.trenutna_orijentacija = ciljni_ugao
         self.get_logger().info(f"Robot promenio pravac na: {ciljni_ugao}°")
@@ -68,25 +71,27 @@ class NametnutoKretanjeNode(Node):
 
         # --- SLUČAJ 1: Kretanje duž X ose ---
         if razlika_x != 0:
-            # Odredi gde robot treba da gleda
             potreban_ugao = 0 if razlika_x > 0 else 180
             self.okreni_robota(potreban_ugao)
             
-            # Izračunaj korake za pravo i pošalji komandu
+            # Izračunaj korake za pravo
             stepovi_pravo = abs(razlika_x) / MM_PER_STEP
-            self.posalji_komandu(stepovi_pravo, stepovi_pravo)
+            
+            # HARDVERSKO PRAVILO: Levi točak (-), Desni točak (+) za kretanje napred
+            self.posalji_komandu(-stepovi_pravo, stepovi_pravo)
             
             self.trenutno_x = ciljno_x
 
         # --- SLUČAJ 2: Kretanje duž Y ose ---
         elif razlika_y != 0:
-            # Odredi gde robot treba da gleda
             potreban_ugao = 90 if razlika_y > 0 else 270
             self.okreni_robota(potreban_ugao)
             
-            # Izračunaj korake za pravo i pošalji komandu
+            # Izračunaj korake za pravo
             stepovi_pravo = abs(razlika_y) / MM_PER_STEP
-            self.posalji_komandu(stepovi_pravo, stepovi_pravo)
+            
+            # HARDVERSKO PRAVILO: Levi točak (-), Desni točak (+) za kretanje napred
+            self.posalji_komandu(-stepovi_pravo, stepovi_pravo)
             
             self.trenutno_y = ciljno_y
 
